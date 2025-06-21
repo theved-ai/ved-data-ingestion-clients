f54f0d9233f4f1b21638f6bb50d2a581f2d1b1db
Badal Patial <badal1906@gmail.com>
2025-06-22 01:36:04 +0530

    some changes in design

--- pensieve-desktop/src/App.jsx
+++ pensieve-desktop/src/App.jsx
@@ -408,9 +408,7 @@ const Widget = ({ note, handleNoteChange, handleSend, handleClose, onToggle, isA
             {isSubmitting ? (
               <div className="spinner"></div>
             ) : (
-              <svg width="20" height="20" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-                <path d="M2.01 21L23 12L2.01 3L2 10L17 12L2 14L2.01 21Z" fill="white" />
-              </svg>
+              <img src="../src/assets/forward-arrow.svg" alt="Send" />
             )}
           </button>
         </div>
