9b222fd71ada63ab884d2c5a87631571a4f62ee0
Simranjeet-Cars24 <simranjeet.patial@cars24.com>
2025-06-23 00:01:59 +0530

    change

--- pensieve-desktop/src/orb.jsx
+++ pensieve-desktop/src/orb.jsx
@@ -1,10 +1,18 @@
-import React from 'react';
-import './orb.css';
+import React from "react";
+import "./orb.css";
 
 const Orb = ({ handleClose, isAnimating }) => (
-  <div id="orb" className={isAnimating ? 'shrink-out' : 'expand-in'}>
+  <div id="orb" className={isAnimating ? "shrink-out" : "expand-in"}>
     <div id="orb-icon" />
-    <button id="close-orb" onClick={(e) => { e.stopPropagation(); handleClose(); }}>x</button>
+    <button
+      id="close-orb"
+      onClick={(e) => {
+        e.stopPropagation();
+        handleClose();
+      }}
+    >
+      X
+    </button>
   </div>
 );
 
